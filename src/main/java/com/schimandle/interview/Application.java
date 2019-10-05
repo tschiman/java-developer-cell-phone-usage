a4e5e76246e35ac43907e8bb4038e7faa27afb46
Tim Schimandle <tims@familytv.com>
2019-10-05 05:51:53 -0600

    print the header

--- src/main/java/com/schimandle/interview/Application.java
+++ src/main/java/com/schimandle/interview/Application.java
@@ -1,6 +1,8 @@
 package com.schimandle.interview;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class Application {
@@ -15,7 +17,16 @@ public class Application {
 
         //build header data:
         // run date, #phones, total mins, total data, avg min, avg data
+        LocalDate runDate = LocalDate.now();
+        int phoneCount = cellPhones.size();
 
+        int totalMinutes = phoneUsages.stream().map(u -> u.minutes).mapToInt(m -> m).sum();
+        BigDecimal totalData = phoneUsages.stream().map(u -> u.data).reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal averageMinutes = new BigDecimal(totalMinutes).divide(new BigDecimal(phoneUsages.size()));
+        BigDecimal averageData = totalData.divide(new BigDecimal(phoneUsages.size()));
+
+        System.out.println(String.format(" - Date %s #Phone: %s totalMins: %s totalData: %s avgMins: %s avgData: %s", runDate, phoneCount, totalMinutes, totalData, averageMinutes, averageData));
 
 
     }
